36a56a5ad1a6c0369dbbbe031431eed2b0ed3201
KamilySAC <164411516+KamilySAC@users.noreply.github.com>
2025-11-24 10:37:38 -0300

    Add files via upload

---
+++ persistencia/crudMotorista.py
@@ -0,0 +1,38 @@
+
+from connector import *
+
+# CRIA MOTORISTA
+
+def cadastrar_motorista(cnh, nome, telefone, jornada):
+    comando = f'INSERT INTO motorista (CNH, nome, telefone, jornada) VALUES ("{cnh}", "{nome}","{telefone}",{jornada})'
+    comandoEscrita(comando)
+
+
+   
+# LISTAR MOTORISTAS
+
+def listarMotoristas():
+    comando = 'SELECT * FROM motorista'
+    return comandoLeitura(comando)
+
+
+# ATUALIZAR MOTORISTA
+
+
+def editar_motorista(cnh, nome=None, telefone=None, jornada=None):
+    if nome is not None:
+        comando = f'UPDATE motorista SET nome = "{nome}" WHERE CNH = "{cnh}"'
+        comandoEscrita(comando)
+    if telefone is not None:
+        comando = f'UPDATE motorista SET telefone = "{telefone}" WHERE CNH = "{cnh}"'
+        comandoEscrita(comando)
+    if jornada is not None:
+        comando = f'UPDATE motorista SET jornada = {jornada} WHERE CNH = "{cnh}"'
+        comandoEscrita(comando)
+
+
+# REMOVER MOTORISTA
+
+def deletar_motorista(cnh):
+    comando = f'DELETE FROM motorista WHERE CNH = "{cnh}"'
+    comandoEscrita(comando)
---
+++ persistencia/crudPassageiro.py
@@ -0,0 +1,43 @@
+from connector import *
+
+
+def listarPassageiros():
+    comando = 'SELECT * FROM passageiro'
+    return comandoLeitura(comando)
+
+
+# CADASTRA PASSAGEIROS
+def cadastrar_passageiro(cpf, nome, telefone, email, data_nascimento):
+    comando = (
+        f'INSERT INTO passageiro (CPF, nome, telefone, email, dataNascimento) '
+        f'VALUES ("{cpf}", "{nome}", "{telefone}", "{email}", "{data_nascimento}")'
+    )
+    comandoEscrita(comando)
+
+
+# LE PASSAGEIROS
+
+def lerPassageiro(cpf):
+    comando = f'SELECT * FROM passageiro WHERE CPF="{cpf}"'
+    return comandoLeitura(comando)
+
+# DELETA PASSGAEIRO
+def deletar_passageiro(cpf):
+    comando = f'DELETE FROM passageiro WHERE CPF="{cpf}"'
+    comandoEscrita(comando)
+
+
+# UPDATES PASSAGEIROS
+def editar_passageiro(cpf, nome=None, telefone=None, email=None, data_nascimento=None):
+    if nome is not None:
+        comando = f'UPDATE passageiro SET nome = "{nome}" WHERE CPF = "{cpf}"'
+        comandoEscrita(comando)
+    if telefone is not None:
+        comando = f'UPDATE passageiro SET telefone = "{telefone}" WHERE CPF = "{cpf}"'
+        comandoEscrita(comando)
+    if email is not None:
+        comando = f'UPDATE passageiro SET email = "{email}" WHERE CPF = "{cpf}"'
+        comandoEscrita(comando)
+    if data_nascimento is not None:
+        comando = f'UPDATE passageiro SET dataNascimento = "{data_nascimento}" WHERE CPF = "{cpf}"'
+        comandoEscrita(comando)
