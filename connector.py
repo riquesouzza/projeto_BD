3687df6ce798299c1aaac0ddc401a7409a9f5ae1
Henrique Souza <165195445+riquesouzza@users.noreply.github.com>
2025-11-30 17:14:16 -0300

    Deixando erro mais amigavel
    Add error handling for database command execution

--- connector.py
+++ connector.py
@@ -11,10 +11,24 @@ def conexaoBD():
 def comandoEscrita(comando):
     conexao = conexaoBD()
     cursor = conexao.cursor()
-    cursor.execute(comando)
-    conexao.commit()
-    cursor.close()
-    conexao.close()
+    try:
+        cursor.execute(comando)
+        conexao.commit()
+
+    except Exception as e:
+        if e.errno == 1451 or e.errno == 1217:
+            raise Exception(
+                "Não é possível excluir/alterar porque existem registros dependentes."
+            ) from e
+
+        if e.errno == 1452 or e.errno == 1216:
+            raise Exception(
+                "Você tentou inserir/alterar um valor que não existe na tabela relacionada."
+            ) from e
+
+    finally:
+        cursor.close()
+        conexao.close()
 
 def comandoEscritaParametros(comando, parametros):
     conexao = conexaoBD()
